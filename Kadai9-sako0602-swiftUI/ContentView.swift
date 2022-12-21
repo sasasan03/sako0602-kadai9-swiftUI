4fc6f61b8d503a02dd21712c58b4dcab65689901
sasasan03 <sako.hiro62@gmail.com>
2022-12-21 21:02:12 +0900

    Initial Commit

---
+++ Kadai9-sako0602-swiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kadai9-sako0602-swiftUI
+//
+//  Created by sako0602 on 2022/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
